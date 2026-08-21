94106fc3236f1128a11198a729eddfbbae6eadc4
Cem Hortoğlu <cemhortoglu@gmail.com>
2026-08-21 19:36:00 +0300

    dassad

--- Pika/Pika.Web.Tests/obj/Debug/net10.0/Pika.Web.Tests.AssemblyInfo.cs
+++ Pika/Pika.Web.Tests/obj/Debug/net10.0/Pika.Web.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pika.Web.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8378161e909eeb5e5fd09e7492c81d087bffa03f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cceee06613f824c911955ec8fceea5401b8530b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pika.Web.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pika.Web.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
