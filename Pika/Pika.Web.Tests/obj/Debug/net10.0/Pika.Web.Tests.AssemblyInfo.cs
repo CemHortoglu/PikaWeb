35e8c28d119df59dabc105c0a7613bcc0b33b794
Cem Hortoğlu <cemhortoglu@gmail.com>
2026-09-06 22:36:18 +0300

    pika

--- Pika/Pika.Web.Tests/obj/Debug/net10.0/Pika.Web.Tests.AssemblyInfo.cs
+++ Pika/Pika.Web.Tests/obj/Debug/net10.0/Pika.Web.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pika.Web.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cceee06613f824c911955ec8fceea5401b8530b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6b8f480e9201d07c5b74e3fbbaefa8630606a62")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pika.Web.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pika.Web.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
